f012ab2049a0ab7ee3aceb69cfe446f7c94edcdd
WangWang <1422778658@qq.com>
2014-10-26 22:03:01 +0800

    这是一个简单的上位机程序，由于现在正在学习与下位机通讯，暂且推送简易版，以后慢慢升级。

---
+++ MsComDlg.cpp
@@ -0,0 +1,313 @@
+
+// MsComDlg.cpp : 实现文件
+//
+
+#include "stdafx.h"
+#include "MsCom.h"
+#include "MsComDlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 用于应用程序“关于”菜单项的 CAboutDlg 对话框
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+	
+// 对话框数据
+	enum { IDD = IDD_ABOUTBOX };
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+// 实现
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(CAboutDlg::IDD)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CMsComDlg 对话框
+
+
+
+CMsComDlg::CMsComDlg(CWnd* pParent /*=NULL*/)
+	: CDialogEx(CMsComDlg::IDD, pParent)
+	, SendData(_T(""))
+	, RecvData(_T(""))
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CMsComDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_COMBO1, Comnum);
+	DDX_Control(pDX, IDC_COMBO2, Bps);
+	DDX_Control(pDX, IDC_COMBO3, DataBit);
+	DDX_Control(pDX, IDC_COMBO4, StopBit);
+	DDX_Control(pDX, IDC_COMBO5, CheckBit);
+	DDX_Control(pDX, IDC_COMSTA, ComStatc);
+	//  DDX_Control(pDX, IDC_RECVDATA, RecvData);
+
+	//  DDX_Control(pDX, IDC_SENDDATA, SendData);
+	DDX_Text(pDX, IDC_SENDDATA, SendData);
+	DDX_Text(pDX, IDC_RECVDATA, RecvData);
+}
+
+BEGIN_MESSAGE_MAP(CMsComDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDOK, &CMsComDlg::OnBnClickedOk)
+	ON_BN_CLICKED(IDC_CLEAR, &CMsComDlg::OnBnClickedClear)
+	ON_BN_CLICKED(ID_EXIT, &CMsComDlg::OnBnClickedExit)
+	ON_BN_CLICKED(ID_OPEN, &CMsComDlg::OnBnClickedOpen)
+	ON_BN_CLICKED(IDC_SEND, &CMsComDlg::OnBnClickedSend)
+	ON_BN_CLICKED(IDC_BEGINRECV, &CMsComDlg::OnBnClickedBedinrecv)
+	ON_BN_CLICKED(IDC_CLOSE, &CMsComDlg::OnBnClickedClose)
+END_MESSAGE_MAP()
+
+
+// CMsComDlg 消息处理程序
+
+BOOL CMsComDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 将“关于...”菜单项添加到系统菜单中。
+
+	// IDM_ABOUTBOX 必须在系统命令范围内。
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != NULL)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// 设置此对话框的图标。  当应用程序主窗口不是对话框时，框架将自动
+	//  执行此操作
+	SetIcon(m_hIcon, TRUE);			// 设置大图标
+	SetIcon(m_hIcon, FALSE);		// 设置小图标
+
+	ShowWindow(SW_MAXIMIZE);
+
+	// TODO:  在此添加额外的初始化代码
+	CComboBox*ComboBox = (CComboBox*)GetDlgItem(IDC_COMBO1);
+	ComboBox->SetCurSel(0);//串口号初始化为1
+	ComboBox = (CComboBox*)GetDlgItem(IDC_COMBO2);
+	ComboBox->SetCurSel(2);//波特率初始化为9600
+	ComboBox = (CComboBox*)GetDlgItem(IDC_COMBO3);
+	ComboBox->SetCurSel(4);//数据位初始化为8
+	ComboBox = (CComboBox*)GetDlgItem(IDC_COMBO4);
+	ComboBox->SetCurSel(1);//停止位初始化为1
+	ComboBox = (CComboBox*)GetDlgItem(IDC_COMBO5);
+	ComboBox->SetCurSel(0);//校检位初始化为0
+	GetDlgItem(IDC_SEND)->EnableWindow(false);//禁用发送功能
+	CEdit* ComSta = (CEdit*)GetDlgItem(IDC_COMSTA);
+	ComStatc.SetWindowTextW(_T("串口未打开"));
+	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
+}
+
+void CMsComDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// 如果向对话框添加最小化按钮，则需要下面的代码
+//  来绘制该图标。  对于使用文档/视图模型的 MFC 应用程序，
+//  这将由框架自动完成。
+
+void CMsComDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 用于绘制的设备上下文
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 使图标在工作区矩形中居中
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 绘制图标
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+//当用户拖动最小化窗口时系统调用此函数取得光标
+//显示。
+HCURSOR CMsComDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+
+void CMsComDlg::OnBnClickedOk()
+{
+	// TODO:  在此添加控件通知处理程序代码
+	CDialogEx::OnOK();
+}
+
+void CMsComDlg::OnBnClickedClear()
+{
+	// TODO:  在此添加控件通知处理程序代码
+	UpdateData(true);
+	SendData.SetString(_T(""));
+	RecvData.SetString(_T(""));
+	UpdateData(false);
+}
+
+
+void CMsComDlg::OnBnClickedExit()
+{
+	// TODO:  在此添加控件通知处理程序代码
+	CloseWindow();
+}
+
+
+void CMsComDlg::OnBnClickedOpen()
+{
+	// TODO:  在此添加控件通知处理程序代码
+	UpdateData(true);
+	if (m_hComHandle != NULL)
+	{
+		MessageBox(_T("串口正在使用"), _T("提示："), MB_OK);
+		CloseHandle(m_hComHandle);
+		return;
+	}
+	CString Com_Num;
+	Comnum.GetLBText(Comnum.GetCurSel(), Com_Num);
+	m_hComHandle = CreateFile(Com_Num, GENERIC_READ || GENERIC_WRITE,
+		0, 0,OPEN_EXISTING, FILE_FLAG_OVERLAPPED,0);//创建串口
+	if (m_hComHandle == INVALID_HANDLE_VALUE)
+	{
+		MessageBox(_T("串口打开失败"),_T("提示："),MB_OK);
+		return;
+	}
+	else
+	{
+		ComStatc.SetWindowTextW(_T("串口已打开"));
+		GetDlgItem(IDC_SEND)->EnableWindow(true);
+	}
+	SetupComm(m_hComHandle,512,1204);//启动串口
+	DCB dcb;
+	CString str_Bps;
+	CString str_DataBit;
+	CString str_StopBit;
+	CString str_CheckBit;
+	Bps.GetLBText(Bps.GetCurSel(), str_Bps);
+	DataBit.GetLBText(DataBit.GetCurSel(), str_DataBit);
+	StopBit.GetLBText(StopBit.GetCurSel(), str_StopBit);
+	CheckBit.GetLBText(CheckBit.GetCurSel(), str_CheckBit);
+	GetCommState(m_hComHandle,&dcb);//获取串口设备信息
+	dcb.BaudRate =_ttoi(str_Bps);//波特率
+	dcb.ByteSize = _ttoi(str_DataBit);// 数据位
+	dcb.StopBits = _ttoi(str_StopBit);//停止位
+	dcb.Parity = _ttoi(str_CheckBit);//校验位
+	SetCommState(m_hComHandle,&dcb);
+	PurgeComm(m_hComHandle,PURGE_TXCLEAR|PURGE_RXCLEAR);
+	UpdateData(false);
+}
+
+
+void CMsComDlg::OnBnClickedSend()
+{
+	UpdateData(true);
+	OVERLAPPED oWrite;//重叠变量
+	BOOL bWrite;
+	memset(&oWrite, 0, sizeof(OVERLAPPED));//初始化重叠变量
+	oWrite.hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);//创建写串口事件
+	DWORD dwWrite = SendData.GetLength();
+	bWrite = WriteFile(m_hComHandle,(char*)(LPCTSTR)SendData,SendData.GetLength(),&dwWrite,&oWrite);
+	if (!bWrite)
+	{
+		if (GetLastError()==ERROR_IO_PENDING)
+		{
+			WaitForSingleObject(oWrite.hEvent,1000);
+		}
+	}
+	UpdateData(false);
+}
+void CMsComDlg::RecvThread()
+{
+	UpdateData(true);
+	OVERLAPPED oRead;
+	BOOL bRead;
+	memset(&oRead,0,sizeof(OVERLAPPED));
+	oRead.hEvent = CreateEvent(NULL,TRUE,FALSE,NULL);
+	char *cRecvData;
+	cRecvData = new char[1024];
+	memset(cRecvData, 0, 1024);
+	DWORD dwRead = 1024;
+	bRead = ReadFile(m_hComHandle,cRecvData,dwRead,&dwRead,&oRead);
+	if (!bRead)
+	{
+		if (GetLastError() == ERROR_IO_PENDING)
+		{
+			WaitForSingleObject(oRead.hEvent, 1000);
+		}
+	}
+	RecvData+=cRecvData;
+	PurgeComm(m_hComHandle, PURGE_TXCLEAR | PURGE_RXCLEAR);
+	UpdateData(false);
+}
+
+void CMsComDlg::OnBnClickedBedinrecv()
+{
+	// TODO:  在此添加控件通知处理程序代码
+	RecvThread();
+
+}
+
+
+void CMsComDlg::OnBnClickedClose()
+{
+	// TODO:  在此添加控件通知处理程序代码
+	GetDlgItem(IDC_SEND)->EnableWindow(false);
+	CloseHandle(m_hComHandle);
+	m_hComHandle = NULL;
+	ComStatc.SetWindowTextW(_T("串口已关闭"));
+}
---
+++ MsComDlg.h
@@ -0,0 +1,54 @@
+
+// MsComDlg.h : 头文件
+//
+
+#pragma once
+#include "afxwin.h"
+
+
+// CMsComDlg 对话框
+class CMsComDlg : public CDialogEx
+{
+// 构造
+public:
+	CMsComDlg(CWnd* pParent = NULL);	// 标准构造函数
+	HANDLE m_hComHandle;
+// 对话框数据
+	enum { IDD = IDD_MSCOM_DIALOG };
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedOk();
+	afx_msg void OnBnClickedClear();
+	afx_msg void OnBnClickedExit();
+	afx_msg void OnBnClickedOpen();
+	void RecvThread();
+	CComboBox Comnum;
+	CComboBox Bps;
+	CComboBox DataBit;
+	CComboBox StopBit;
+	CComboBox CheckBit;
+
+	CEdit ComStatc;
+	// 接收数据
+//	CEdit RecvData;
+	// 要发送的数据
+	afx_msg void OnBnClickedSend();
+	CString SendData;
+	afx_msg void OnBnClickedBedinrecv();
+	// 接收到的数据
+	CString RecvData;
+	afx_msg void OnBnClickedClose();
+};
